8c9acc5d41e8295fada0e76ca9aa842a2997e250
Devnex-13 <devanshu.p.pote@gmail.com>
2025-03-02 15:50:19 +0530

    Add Condition When We Call Jarvis Give Reply as Yes

--- mainCode.py
+++ mainCode.py
@@ -10,18 +10,23 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+          
 if __name__=="__main__":
     speak("Hello, I am JARVIS. Your Personnel Assistance. How can i help You?")
 
     while True:
         r = sr.Recognizer()
+        
 
-        with sr.Microphone() as source:
-            print("Listening...")
-            print(source)
-            audio = r.listen(source, phrase_time_limit=2)
         try:
-            word = r.recognize_google(audio)
-            print(word)
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=2)
+            command = r.recognize_google(audio)
+            print(command)
+
+            if(command.lower()=="jarvis"):
+                speak("Yes")
+                
         except Exception as e:
             print("Error {e};")
