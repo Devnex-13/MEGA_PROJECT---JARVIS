5da916794bc2a3856c1fa58f474a5f3cd8a6f51a
Devnex-13 <devanshu.p.pote@gmail.com>
2025-03-04 01:31:10 +0530

    Add Comment file To Use gtts module for more good voice

---
+++ Speak_With_gTTs_Lib.py
@@ -0,0 +1,30 @@
+# Only Run In Python below 3.10 version
+# Remove Comment mar and run code in below python version.
+# Run Successfully. 
+
+
+# from gtts import gTTS
+# import pygame
+# import os
+# text = "help me"
+# # def speak(text):
+# tts = gTTS(text)
+# tts.save("temp.mp3")
+
+#     # Initialize the mixer module
+# pygame.mixer.init()
+
+#     # unLoad the MP3 file
+# pygame.mixer.music.load("temp.mp3")
+
+#     # Play the music
+# pygame.mixer.music.play()
+
+#     # Keep the program running so that the music can play
+# while pygame.mixer.music.get_busy():  # While music is playing
+#     pygame.time.Clock().tick(10)  # Check every 10ms to keep the program running
+
+#     # When the music finishes, you can optionally stop the mixer
+# pygame.mixer.music.stop()
+
+# os.remove("temp.mp3")
